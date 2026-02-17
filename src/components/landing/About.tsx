101b94817883c91cf4eaa1f910f568f5d26f809e
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-17 07:50:06 +0700

    [update] about components and config

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -1,10 +1,9 @@
-import { about, mySkills } from '@/config/About';
+import { about } from '@/config/About';
 import Image from 'next/image';
 import React from 'react';
 
 import Container from '../common/Container';
 import SectionHeading from '../common/SectionHeading';
-import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
 
 export default function About() {
   return (
@@ -13,28 +12,15 @@ export default function About() {
       {/* About me */}
       <div className="mt-8 flex flex-col gap-4 md:flex-row">
         <Image
-          src="/assets/logo.png"
+          src="/assets/me.jpg"
           alt="About"
-          width={100}
-          height={100}
-          className="border-secondary size-60 rounded-md border-2 bg-blue-300 dark:bg-yellow-300"
+          width={500}
+          height={500}
+          className="h-60 w-48 rounded-md border-2 border-secondary bg-blue-300 object-cover dark:bg-yellow-300"
         />
         <div className="mt-4">
           <h3 className="text-2xl font-bold">{about.name}</h3>
           <p className="text-secondary mt-4">{about.description}</p>
-          <p className="text-secondary mt-8 font-bold">Skills</p>
-          <div className="flex flex-wrap gap-2">
-            {mySkills.map((skill) => (
-              <Tooltip key={skill.key}>
-                <TooltipTrigger asChild>
-                  <div className="mt-4 size-6 hover:cursor-pointer">
-                    {skill}
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent>{skill.key}</TooltipContent>
-              </Tooltip>
-            ))}
-          </div>
         </div>
       </div>
     </Container>
--- src/config/About.tsx
+++ src/config/About.tsx
@@ -1,26 +1,35 @@
-import Bun from '@/components/technologies/Bun';
+import CSS from '@/components/technologies/CSS';
+import Express from '@/components/technologies/Express';
+import Html from '@/components/technologies/Html';
 import JavaScript from '@/components/technologies/JavaScript';
+import Laravel from '@/components/technologies/Laravel';
 import MongoDB from '@/components/technologies/MongoDB';
+import MySQL from '@/components/technologies/MySQL';
 import NextJs from '@/components/technologies/NextJs';
 import NodeJs from '@/components/technologies/NodeJs';
 import PostgreSQL from '@/components/technologies/PostgreSQL';
-import Prisma from '@/components/technologies/Prisma';
+// import Postman from '@/components/technologies/Postman';
 import ReactIcon from '@/components/technologies/ReactIcon';
 import TypeScript from '@/components/technologies/TypeScript';
+import WordPress from '@/components/technologies/WordPress';
 
 export const mySkills = [
-  <ReactIcon key="react" />,
-  <Bun key="bun" />,
+  <Html key="html" />,
+  <CSS key="css" />,
   <JavaScript key="javascript" />,
   <TypeScript key="typescript" />,
-  <MongoDB key="mongodb" />,
+  <ReactIcon key="react" />,
   <NextJs key="nextjs" />,
+  <Laravel key="laravel" />,
+  <Express key="express" />,
+  <MongoDB key="mongodb" />,
   <NodeJs key="nodejs" />,
   <PostgreSQL key="postgresql" />,
-  <Prisma key="prisma" />,
+  <WordPress key="wordpress" />,
+  <MySQL key="mysql" />,
 ];
 
 export const about = {
-  name: 'Ramkrishna Swarnkar',
-  description: `I'm a Full Stack web developer and Open Source Contributor, I love building products to solve real-world problems. I'm specialized in building MVP's.`,
+  name: 'Benedicto Geraldo Doa Dawa',
+  description: `I'm a final-year Information Systems student who loves learning through building real-world projects. I enjoy experimenting, refining details, and transforming ideas into meaningful, interactive experiences. I believe growth comes from staying curious, consistent, and open to feedback and collaboration while continuously improving both technical and problem-solving skills.`,
 };
