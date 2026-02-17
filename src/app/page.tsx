47efac76e72e7045e8d4d409375c3981c6ec5985
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-17 07:51:39 +0700

    [update] main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Container from '@/components/common/Container';
 import About from '@/components/landing/About';
 import CTA from '@/components/landing/CTA';
+import Education from '@/components/landing/Education';
 import Experience from '@/components/landing/Experience';
 import Hero from '@/components/landing/Hero';
 import Work from '@/components/landing/Projects';
+import Technology from '@/components/landing/Technology';
 import React from 'react';
 
 export default function page() {
   return (
-    <Container className="min-h-screen py-16">
+    <Container className="min-h-screen py-13">
       <Hero />
+      <About />
+      <Education />
+      <Technology />
       <Experience />
       <Work />
-      <About />
       <CTA />
     </Container>
   );
