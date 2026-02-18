ba5372363d567bb3463b4a3c197e1c5353fe7a77
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-19 00:30:24 +0700

    [fix] error production on logo upnvj

--- src/config/Education.tsx
+++ src/config/Education.tsx
@@ -3,5 +3,5 @@ export const educationConfig = {
   major: 'S1 Sistem Informasi',
   period: '2022 - Now',
   gpa: '3.8 / 4.0',
-  logo: '/assets/logo_UPNVJ.png',
+  logo: '/assets/logo-upnvj.png',
 };
