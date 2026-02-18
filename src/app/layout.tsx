3bc4a3b0844abd42632cfc3f62eb2a88d6b47fe1
BenedictoGeraldo <geraldodawa@gmail.com>
2026-02-19 00:12:45 +0700

    [update] main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
+import EntranceAnimation from '@/components/common/EntranceAnimation';
 import Footer from '@/components/common/Footer';
 import Navbar from '@/components/common/Navbar';
 import { generateMetadata as getMetadata } from '@/config/Meta';
 import ReactLenis from 'lenis/react';
 import { ViewTransitions } from 'next-view-transitions';
+import { Cinzel } from 'next/font/google';
 
 import './globals.css';
 
 export const metadata = getMetadata('/');
+const cinzel = Cinzel({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-cinzel',
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +23,8 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="en" className="dark" suppressHydrationWarning>
-        <body className={`font-hanken-grotesk antialiased`}>
+        <body className={` ${cinzel.variable} font-hanken-grotesk antialiased`}>
+          <EntranceAnimation />
           <ReactLenis root>
             <Navbar />
             {children}
